4929e6d94b4f5f6a3a117915a86e9e7f7f6e32d9
SAEED.PANAHI <panahi.projects@gmail.com>
2025-07-15 12:56:14 +0330

    chore: add clicky script and link to analyze the page

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,7 +1,7 @@
-import React, {type ReactNode} from 'react';
-import Layout from '@theme-original/Layout';
-import type LayoutType from '@theme/Layout';
-import type {WrapperProps} from '@docusaurus/types';
+import React, { type ReactNode } from "react";
+import Layout from "@theme-original/Layout";
+import type LayoutType from "@theme/Layout";
+import type { WrapperProps } from "@docusaurus/types";
 
 type Props = WrapperProps<typeof LayoutType>;
 
@@ -9,6 +9,22 @@ export default function LayoutWrapper(props: Props): ReactNode {
   return (
     <>
       <Layout {...props} />
+      {/* Clicky Analytics */}
+      <a
+        title="Privacy-friendly Web Analytics"
+        href="https://clicky.com/101488604"
+      >
+        <img
+          alt="Clicky"
+          src="//static.getclicky.com/media/links/badge.gif"
+          style={{ border: "none" }}
+        />
+      </a>
+      <script
+        async
+        data-id="101488604"
+        src="//static.getclicky.com/js"
+      ></script>
     </>
   );
 }
